8d7101c791210cb46f3410e611920b634b4be397
Ali Poorabbas <ali.saba1396@gmail.com>
2021-11-24 02:34:38 +0330

    Add files via upload

---
+++ fraction.py
@@ -0,0 +1,53 @@
+import math
+
+class Fract:
+    def __init__(self,u,d):
+        self.u = u
+        self.d = d
+    
+    def show(self):
+        print(self.u , '/' , self.d)
+    
+    def simp(self , other):
+        sim = math.gcd(other.u , other.d)
+        other.u = other.u // sim
+        other.d = other.d // sim
+        return other
+
+    def sum(self , other):
+        return self.simp(Fract(self.u * other.d + self.d * other.u , self.d * other.d))
+
+    def sub(self , other):
+        return self.simp(Fract(self.u * other.d - self.d * other.u , self.d * other.d))
+
+    def mul(self , other):
+        return self.simp(Fract(self.u * other.u , self.d * other.d))
+
+    def div(self , other):
+        return self.simp(Fract(self.u * other.d , self.d * other.u))
+
+
+a_up = int(input('Enter first up : '))
+a_down = int(input('Enter first down : '))
+x = Fract(a_up , a_down)
+
+b_up = int(input('Enter second up : '))
+b_down = int(input('Enter second down : '))
+y = Fract(b_up , b_down)
+
+print('INPUT first & second :')
+x.show()
+y.show()
+
+print('RESULT + & - & * & / :')
+plus = x.sum(y)
+plus.show()
+
+mines = x.sub(y)
+mines.show()
+
+mult = x.mul(y)
+mult.show()
+
+divide = x.div(y)
+divide.show()
