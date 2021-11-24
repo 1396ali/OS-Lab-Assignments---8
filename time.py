a9c0706789c551c4553467edcd8ba37b559999f6
Ali Poorabbas <ali.saba1396@gmail.com>
2021-11-24 19:52:42 +0330

    Update time.py

--- time.py
+++ time.py
@@ -9,7 +9,6 @@ class Time:
     def show(self):
         print("[", self.h ,":", self.m ,":", self.s , "]")
 
-
     def correct(self):
         while self.s >= 60:
             self.s -= 60
@@ -28,7 +27,6 @@ class Time:
             self.m += 60
             self.h -= 1
 
-
     def sum(self , other):
         return Time(self.h + other.h , self.m + other.m , self.s + other.s)
 
@@ -40,19 +38,39 @@ class Time:
        print(res , "ثانیه")
        return res
 
+
 print('Times are (T1 _ T2 _ T3 _ second) : ')
-t1 = Time(17,60,60)
+
+t1_h = int(input('Enter hour time : '))
+t1_m = int(input('Enter minute time : '))
+t1_s = int(input('Enter second time : '))
+
+t1 = Time(t1_h,t1_m,t1_s)
 t1.show()
 
-t2 = Time(0,59,60)
+
+t2_h = int(input('Enter hour time : '))
+t2_m = int(input('Enter minute time : '))
+t2_s = int(input('Enter second time : '))
+
+t2 = Time(t2_h,t2_m,t2_s)
 t2.show()
 
-t3 = Time(18,2,60)
+
+t3_h = int(input('Enter hour time : '))
+t3_m = int(input('Enter minute time : '))
+t3_s = int(input('Enter second time : '))
+
+t3 = Time(t3_h,t3_m,t3_s)
 t3.show()
 
-time = Time(0,0,3600)
 
-print('RESULT is (T1+T2 _ T2+T3 / T3-T1 _ T1-T2 / second->time _ time->second) :')
+time_second = int(input('Enter second : '))
+
+time = Time(0,0,time_second)
+
+
+print('RESULT is (T1+T2 _ T2+T3 / T2-T1 _ T3-T2 / second(input)->time _ time(T3)->second) :')
 
 a = t1.sum(t2)
 a.show()
@@ -62,14 +80,14 @@ b.show()
 
 print('/')
 
-c = t3.sub(t1)
+c = t2.sub(t1)
 c.show()
 
-d = t1.sub(t2)
+d = t3.sub(t2)
 d.show()
 
 print('/')
 
 time.show()
 
-t2.time_to_sec()
+t3.time_to_sec()
